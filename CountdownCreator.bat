2817a463b0eb60d17eeee7581417627924e9c936
David Grindley <david@fieryfred.org.uk>
2022-05-23 20:20:25 +0100

    Added more configuration of the various steps Added support for files with spaces

--- CountdownCreator.bat
+++ CountdownCreator.bat
@@ -1,40 +1,65 @@
-rem @echo off
+@echo off
 setlocal
 
 rem
 rem Generate the output file name
 rem
-for %%A in (%1) do (set FILENAME=%%~nA)
+for %%f in (%1) do (set FILENAME=%%~nf)
 
-if not defined VIDEOSDIR (set VIDEOSDIR=%HOMEDRIVE%%HOMEPATH%\Videos)
+if not defined VIDEODIR (set VIDEODIR="%HOMEDRIVE%%HOMEPATH%\Videos")
 
-set TARGETPATH=%VIDEOSDIR%\%FILENAME%.mp4
+set TARGETPATH="%VIDEODIR%\%FILENAME%.mp4"
+
 
 rem
 rem Standard font values for the call to FFMPEG
 rem 
-set DURATION=30
 set FONTCOLOUR=fontcolor=white
 set FONTBORDER=borderw=2:bordercolor=black
 set FONTFILE=fontfile='c\:\\windows\\fonts\\Arialbd.ttf'
 
-set FADESTART=%DURATION%-20
-set FADEEND=%DURATION%-15
+rem
+rem Parameters affecting the fade of the countdown text
+rem all of them are in seconds
+rem
+set COUNTDOWN_DURATION=900
+set LARGECOUNTCOUNTDOWN_DURATION=30
+set FADECOUNTDOWN_DURATION=5
 
-set SMALLTEXT=text='Starting in %%{eif\:(%DURATION%-t)/60\:d\:2}m %%{eif\:mod(%DURATION%-t, 60)\:d\:2}s'
+set /a FADESTART=%COUNTDOWN_DURATION%-%LARGECOUNTCOUNTDOWN_DURATION%
+set /a FADEEND=%FADESTART%+5
+
+rem The text showing the time left displayed in the bottom right hand corner, inset by 10% of the output width
+set SMALLTEXT=text='Starting in %%{eif\:(%COUNTDOWN_DURATION%-t)/60\:d\:2}m %%{eif\:mod(%COUNTDOWN_DURATION%-t, 60)\:d\:2}s'
 set SMALLFONTSIZE=fontsize=40
 set SMALLXPOS=x=(w-text_w)-(0.1*w)
 set SMALLYPOS=y=(h-text_h)-(0.1*h)
+
+rem Hide the text at the end of the fade
 set SMALLENABLE=enable='lt(t,%FADEEND%)'
-set SMALLALPHA=alpha='if(lt(t,%FADESTART%),1,(%FADEEND%-t)/5)'
+
+rem Use the Alpha channel to produce the fade
+set SMALLALPHA=alpha='if(lt(t,%FADESTART%),1,(%FADEEND%-t)/%FADECOUNTDOWN_DURATION%)'
 set SMALLCOUNTDOWN=drawtext=%SMALLTEXT%:%FONTFILE%:%SMALLFONTSIZE%:%FONTCOLOUR%:%SMALLXPOS%:%SMALLYPOS%:%FONTBORDER%:%SMALLENABLE%:%SMALLALPHA%
 
-set LARGETEXT=text='%%{eif\:mod(%DURATION%-t, 60)\:d}'
+rem The big counter text that fades in for the last portion of the countdown, displayed
+rem in the center of the video
+set LARGETEXT=text='%%{eif\:mod(%COUNTDOWN_DURATION%-t, 60)\:d}'
 set LARGEFONTSIZE=fontsize=400
 set LARGEXPOS=x=(w-text_w)/2
 set LARGEYPOS=y=(h-text_h)/2
-set LARGEENABLE=enable='between(t,%FADESTART%,%DURATION%-1)'
+
+rem Remove the counter at the end of the countdown
+set LARGEENABLE=enable='between(t,%FADESTART%,%COUNTDOWN_DURATION%-1)'
 set LARGEALPHA=alpha='if(gt(t,%FADEEND%),0.5,(t-(%FADESTART%))/10)'
 set LARGECOUNTDOWN=drawtext=%LARGETEXT%:%FONTFILE%:%LARGEFONTSIZE%:%FONTCOLOUR%:%LARGEXPOS%:%LARGEYPOS%:%FONTBORDER%:%LARGEENABLE%:%LARGEALPHA%
 
-%FFMPEG_PATH%\ffmpeg -loop 1 -t %DURATION% -i %1 -y -vf "fps=5,format=yuv420p,%SMALLCOUNTDOWN%,%LARGECOUNTDOWN%" -c:v libx264 %TARGETPATH%
+rem
+rem Set the path for FFMPEG
+rem
+
+if defined FFMPEGPATH (set PATH=%FFMPEGPATH%;"%PATH%")
+
+ffmpeg -loop 1 -t %COUNTDOWN_DURATION% -i %1 -y -vf "fps=5,format=yuv420p,%SMALLCOUNTDOWN%,%LARGECOUNTDOWN%" -c:v libx264 %TARGETPATH%
+
+echo Created video: %TARGETPATH%
